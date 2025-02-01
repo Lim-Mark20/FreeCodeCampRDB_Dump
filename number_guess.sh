7677e2450897741d8a6440685b888a015a1404d7
mark lim <mblim1220@gmail.com>
2025-02-01 11:50:33 +0000

    chore: Added a minor comment

--- number_guess.sh
+++ number_guess.sh
@@ -29,6 +29,7 @@ else
   fi
 fi
 
+# Increase guess count before checking if it's correct
 # Generate a secret number between 1 and 1000
 SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
 echo "Guess the secret number between 1 and 1000:"
